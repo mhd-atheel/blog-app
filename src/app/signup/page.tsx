437841cec9dd0381115055f6ac1ff0b5b34649b8
mhd-atheel <aathilmazz1234@gmail.com>
2023-08-06 00:03:30 +0530

    create signup api and calling

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,43 +1,59 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
 const Signup = () => {
     const router = useRouter();
     const [data, setData] = useState({
-        name: "",
+        username: "",
         email: "",
         password: "",
     });
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
     const [emailValid, setEmailValid] = useState(false);
     const [passwordValid, setPasswordValid] = useState(false);
     const [nameValid, setNameValid] = useState(false);
 
-    const handleChange = () => {
-        if (data.name.length <= 0) {
-            setNameValid(true);
-        } else if (data.name.length >= 0) {
-            setNameValid(false);
+    // const handleChange = () => {
+    //     if (data.name.length <= 0) {
+    //         setNameValid(true);
+    //     } else if (data.name.length >= 0) {
+    //         setNameValid(false);
+    //     }
+    //     if (data.email.length <= 0) {
+    //         setEmailValid(true);
+    //     } else if (data.email.length >= 0) {
+    //         setEmailValid(false);
+    //     }
+    //     if (data.password.length <= 0) {
+    //         setPasswordValid(true);
+    //     } else if (data.password.length >= 0) {
+    //         setPasswordValid(false);
+    //     }
+    //     if (
+    //         data.email.length > 0 &&
+    //         data.password.length > 0 &&
+    //         data.name.length > 0
+    //     ) {
+    //         router.push("/");
+    //     }
+    // };
+    const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", data);
+            console.log("Signup success", response.data);
+            router.push("/login");
+            
+        } catch (error:any) {
+            console.log("Signup failed", error.message);
+            
+        }finally {
+            setLoading(false);
         }
-        if (data.email.length <= 0) {
-            setEmailValid(true);
-        } else if (data.email.length >= 0) {
-            setEmailValid(false);
-        }
-        if (data.password.length <= 0) {
-            setPasswordValid(true);
-        } else if (data.password.length >= 0) {
-            setPasswordValid(false);
-        }
-        if (
-            data.email.length > 0 &&
-            data.password.length > 0 &&
-            data.name.length > 0
-        ) {
-            router.push("/");
-        }
-    };
-
+    }
     return (
         <div className="flex justify-center items-center m-auto h-screen bg-slate-900">
             <div className="w-6/12 h-96 rounded-3xl justify-center items-center flex-col flex ">
@@ -53,7 +69,7 @@ const Signup = () => {
                     name="name"
                     id=""
                     placeholder="Your name"
-                    onChange={(e) => setData({ ...data, name: e.target.value })}
+                    onChange={(e) => setData({ ...data, username: e.target.value })}
                 />
                 {nameValid === true ? (
                     <h6 className="text-left text-xs text-red-600 ">please add name </h6>
@@ -92,7 +108,7 @@ const Signup = () => {
                 )}
                 <button
                     className="p-3 rounded-md w-5/12 bg-orange-400 mt-5 font-bold"
-                    onClick={handleChange}
+                    onClick={onSignup}
                 >
                     Register
                 </button>
